73634f688f9652f25a980f7b3624febfc1586309
Elbarky <practicalmonster.itsec@gmail.com>
2019-05-23 11:03:56 +0200

    single component todo list with local storage

---
+++ TodoList/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+    selector: 'app-server', //how to use this component in other files
+    templateUrl: './server.component.html'
+})
+
+
+export class ServerComponent{
+
+}
